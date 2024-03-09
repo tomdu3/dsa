42edd0278107e6a19282a09328fbcfdc509e96ce
Tomislav Dukez <tomdu3@ymail.com>
2024-03-09 11:01:42 +0000

    Fix: graphs

--- python/graphs.py
+++ python/graphs.py
@@ -1,14 +1,15 @@
 class WeightedGraph:
-  def __init__(self):
-    self.vertices = {}
-  
-  def add_vertex(self, vertex):
-    # Set the data for the vertex
-    self.vertices[vertex] = []
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex):
+        # Set the data for the vertex
+        self.vertices[vertex] = []
     
-  def add_edge(self, source, target, weight):
-    # Set the weight
-    self.vertices[source].append([target, weight])
+    def add_edge(self, source, target, weight):
+        # Set the weight
+        self.vertices[source].append([target, weight])
+
 
 my_graph = WeightedGraph()
 
