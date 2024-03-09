c7c3dc289a2659b62b99146b1103e56821b23df5
Tomislav Dukez <tomdu3@ymail.com>
2024-03-09 11:48:34 +0000

    Fix: recursion

---
+++ python/recursion.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+  # Define the base case
+  if n <= 1:
+    return n
+  else:
+    # Call recursively to fibonacci
+    return fibonacci(n-1) + fibonacci(n-2)
+    
+print(fibonacci(6))
