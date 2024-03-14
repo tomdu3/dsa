458413a9215e88d22b97b39a5a54b1d302acdfd4
Tomislav Dukez <tomdu3@ymail.com>
2024-03-14 12:09:17 +0000

    Add Breath First Search graph

---
+++ python/bfs-graph.py
@@ -0,0 +1,23 @@
+import queue
+
+def bfs(graph, initial_vertex, search_value):
+  visited_vertices = []
+  bfs_queue = queue.SimpleQueue()
+  visited_vertices.append(initial_vertex)
+  bfs_queue.put(initial_vertex)
+
+  while not bfs_queue.empty():
+    current_vertex = bfs_queue.get()
+    # Check if you found the search value
+    if current_vertex == search_value:
+      # Return True if you find the search value
+      return True    
+    for adjacent_vertex in graph[current_vertex]:
+      # Check if the adjacent vertex has been visited
+      if adjacent_vertex not in visited_vertices:
+        visited_vertices.append(adjacent_vertex)
+        bfs_queue.put(adjacent_vertex)
+  # Return False if you didn't find the search value
+  ____
+
+print(bfs(graph, '4', '8'))
